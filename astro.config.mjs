0f8579b8ddaba1458c405667b1cf0daa25be4c94
Victor Melia Vilar <90848815+VicMelia@users.noreply.github.com>
2025-11-28 18:13:03 +0100

    Integrate Tailwind CSS with Vite in config
    Added Tailwind CSS plugin to Vite configuration.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://vicmelia.github.io',
-  base: '/victormelia.github.io/',
+ site: 'https://vicmelia.github.io',
+ vite: {
+   plugins: [tailwindcss()],
+ }
 });
